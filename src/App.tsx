f49d16ac126e47ba85154d69f92d3554551b8497
defreezz <yatsevich-artsiom@yandex.by>
2021-12-11 13:30:17 +0300

    v4

--- src/App.tsx
+++ src/App.tsx
@@ -11,56 +11,104 @@ export type TaskType = {
 
 export type FilterValuesType = "all" | "active" | "completed"
 
+type TodoListType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+type TaskStateType = {
+    [key: string]: TaskType[]
+}
+
 function App() {
-
     //BLL
-    const tdlTitle = "What to learn"
+    const todolistID_1 = v1()
+    const todolistID_2 = v1()
 
-    //инициализация state
-    //useState возвращает массив и функцию (которая сравнивает измененный массив с исходным массивом)
-    const [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: v1(), title: "HTML", isDone: false},
-        {id: v1(), title: "Css", isDone: false},
-        {id: v1(), title: "Redux", isDone: true}
+    const [todoLists, setTodoLists] = useState<TodoListType[]>([
+        {id: todolistID_1, title: 'What to learn', filter: "all"},
+        {id: todolistID_2, title: 'What to buy', filter: "all"},
     ])
-    //Удаление таски
-    const removeTask = (taskID: string) => setTasks(tasks.filter(t => t.id !== taskID))
-    // const tasks = result[0] // возврат массива из useState
-    // const setTasks = result[1] // передаем в функцию массив, который сравнивается с исходным массивом в useState
 
-    //Фильрация такски
-    const [filter, setFilter] = useState<FilterValuesType>("all")
-    const changeFilter = (filter: FilterValuesType) => {
-        setFilter(filter)
+    const [tasks, setTasks] = useState<TaskStateType>({
+        [todolistID_1]: [
+            {id: v1(), title: "HTML", isDone: false},
+            {id: v1(), title: "Css", isDone: false},
+            {id: v1(), title: "Redux", isDone: true}
+        ],
+        [todolistID_2]: [
+            {id: v1(), title: "Milk", isDone: false},
+            {id: v1(), title: "Bear", isDone: false},
+            {id: v1(), title: "Fish", isDone: true}
+        ]
+    })
+
+    //ф-ция для изменения чекбокса (выполненная/невыполненная) таска
+    const changeTaskStatus = (taskID: string, isDone: boolean, todolistID:string) => {
+        const copyState = {...tasks}
+        copyState[todolistID] = copyState[todolistID].map(t => t.id === taskID? {...t,isDone}:t)
+        setTasks(copyState)
+
+        // let task = tasks[todolistID].find(t => t.id === taskID)
+        // if (task) {
+        //     task.isDone = isDone
+        //     setTasks({...tasks})
+        }
+
+
+
+    //Фильрация таски
+    const changeFilter = (filter: FilterValuesType, todolistID: string) => {
+        const updatedTodoLists = todoLists.map(tdl => tdl.id === todolistID ? {...tdl, filter: filter} : tdl)
+        setTodoLists(updatedTodoLists)
     }
 
-    let tasksForRender = tasks
-    if (filter === "active") {
-        tasksForRender = tasks.filter(t => t.isDone === false)
-    }
-    if (filter === "completed") {
-        tasksForRender = tasks.filter(t => t.isDone === true)
-    }
 
     //Добавление таски
-    function addTask(title:string) {
+    function addTask(title: string, todolistID: string) {
+        const copyState = {...tasks}
         let inputTask = {id: v1(), title: title, isDone: false}
-        let newTask = [inputTask, ...tasks]
-        setTasks(newTask)
+        copyState[todolistID] = [inputTask, ...copyState[todolistID]]
+        setTasks(copyState)
     }
+
+    //Удаление таски
+    const removeTask = (taskID: string, todolistID: string) => {
+        const copyState = {...tasks}
+        copyState[todolistID] = copyState[todolistID].filter(t => t.id !== taskID)
+
+        setTasks(copyState)
+    }
+
+    //map для тудулистОВ
+    const todoListRender = todoLists.map((tdl) => {
+            let tasksForRender = tasks[tdl.id]
+            if (tdl.filter === "active") {
+                tasksForRender = tasks[tdl.id].filter(t => t.isDone === false)
+            }
+            if (tdl.filter === "completed") {
+                tasksForRender = tasks[tdl.id].filter(t => t.isDone === true)
+            }
+                return (
+                    <ToDoList
+                        key={tdl.id}
+                        id={tdl.id}
+                        title={tdl.title}
+                        tasks={tasksForRender}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeTaskStatus}
+                        filter={tdl.filter}
+                    />)
+            }
+        )
 
     //UI
     return (
         <div className={"App"}>
-            <ToDoList
-                title={tdlTitle}
-                tasks={tasksForRender}
-                removeTask={removeTask}
-                changeFilter={changeFilter}
-                addTask={addTask}
-                filter={filter}
-            />
-
+            {todoListRender}
         </div>
     );
 }
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,61 +2,82 @@ import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterValuesType, TaskType} from "./App";
 
 type TypeOfProps = {
+    id:string
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskID: string) => void
-    changeFilter: (filter: FilterValuesType) => void
-    addTask: (title:string) => void
+    removeTask: (taskID: string, todolistID: string) => void
+    changeFilter: (filter: FilterValuesType, todolistID: string) => void
+    addTask: (title:string, todolistID: string) => void
+    changeTaskStatus: (taskID: string, isDone: boolean, todolistID:string) => void
     filter: FilterValuesType
 }
 
 
-function ToD oList(props: TypeOfProps) {
+function ToDoList(props: TypeOfProps) {
+    //локальный стейт для инпута
+    const [newTaskTittle, setNewTaskTittle] = useState("")
 
+    //стейт для отображения ошибки
+    const [error, setError] = useState<string>("")
+
+    //мапится массив тасок
     const tasksJSX = props.tasks.map(t => {
         return (
             <li key={t.id}  >
-                <input type="checkbox" checked={t.isDone} />
+                <input type="checkbox"
+                       checked={t.isDone}
+                       onChange={(e)=> props.changeTaskStatus(t.id, e.currentTarget.checked, props.id)}
+                />
+
                 <span className={t.isDone ? "completed": ""}>{t.title}</span>
-                <button onClick={() => {props.removeTask(t.id)}}>x</button>
+                <button onClick={() => {props.removeTask(t.id, props.id)}}>x</button>
             </li>)
     })
 
     //обработка событий
     const addTask = () => {
         const trimmedTitle = newTaskTittle.trim()
-            if(trimmedTitle) {props.addTask(trimmedTitle)}
-        setNewTaskTittle(""); }
+            if(trimmedTitle) {
+                props.addTask(trimmedTitle, props.id)
+            }else{
+                setError("Обязательное поле")}
+        setNewTaskTittle("");
+    }
 
-    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {setNewTaskTittle(e.currentTarget.value)}
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTittle(e.currentTarget.value);
+        setError("")
+    }
+
     const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) =>{
         if(e.key === "Enter"){addTask()}
     }
-    const onAllClickHandler = () => {props.changeFilter("all")}
-    const onActiveClickHandler = () => {props.changeFilter("active")}
-    const onCompletedClickHandler = () => {props.changeFilter("completed")}
-    const buttonStatus = (filter:FilterValuesType ) => { return  props.filter === filter ? "activeButton": ""} //меняет класс кнопкам в зависимости от фильтрации в аpp.tsx
+    const onAllClickHandler = () => {props.changeFilter("all",props.id)}
+    const onActiveClickHandler = () => {props.changeFilter("active",props.id)}
+    const onCompletedClickHandler = () => {props.changeFilter("completed",props.id)}
+    const buttonStatusClass = (filter:FilterValuesType ) => { return  props.filter === filter ? "activeButton": ""} //меняет класс кнопкам в зависимости от фильтрации в аpp.tsx
+    const errorClass = (error !== '' ? "errorInput" : "defaultInput")
 
-    //локальный стейт для инпута
-    const [newTaskTittle, setNewTaskTittle] = useState("")
 
     return (
-        <div className="App">
+        <div  className="App">
             <div>
                 <h3>{props.title}</h3>
                 <div>
-                    <input value={newTaskTittle}
+                    <input className={errorClass} value={newTaskTittle}
                            onChange={onChangeHandler}
                            onKeyPress={onKeyPressHandler}/>
+
                     <button onClick={addTask}>+</button>
+                    <div className={"error"}>{error}</div>
                 </div>
                 <ul>
                     {tasksJSX}
                 </ul>
                 <div>
-                    <button onClick={onAllClickHandler} className={buttonStatus("all")}>All</button>
-                    <button onClick={onActiveClickHandler} className={buttonStatus("active")}>Active</button>
-                    <button onClick={onCompletedClickHandler} className={buttonStatus("completed")}>Completed</button>
+                    <button onClick={onAllClickHandler} className={buttonStatusClass("all")}>All</button>
+                    <button onClick={onActiveClickHandler} className={buttonStatusClass("active")}>Active</button>
+                    <button onClick={onCompletedClickHandler} className={buttonStatusClass("completed")}>Completed</button>
                 </div>
             </div>
         </div>
