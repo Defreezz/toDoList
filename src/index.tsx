a03413cb9f1849700239c3f8d9b9755d36784ee8
Artsiom <yatsevich-artsiom@yandex.by>
2022-03-01 12:33:13 +0300

    gh pages added

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,21 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./redux/store/store";
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import {HashRouter, Navigate, Route, Routes} from 'react-router-dom';
 import {Login} from "./Components/Login/Login";
 import {PageNotFound} from "./Components/Utils/PageNotFound/PageNotFound";
 import App from "./App";
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path={"/"} element={<App/>}/>
+                <Route path={"/todolist"} element={<App/>}/>
                 <Route path={"/login"} element={<Login/>}/>
                 <Route path={"/404"} element={<PageNotFound/>} />
                 <Route path={"*"} element={<Navigate to={"/404"}/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>, document.getElementById('root'));
 
 
